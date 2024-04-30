3b08c0c3ecd6231f9ec0b2dab323003f43904f45
Your Name <gracefeng@ucsb.edu>
2024-04-29 20:56:06 -0700

    Edit HelpRequest repository

--- src/main/java/edu/ucsb/cs156/example/repositories/UCSBHelpRequestRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/UCSBHelpRequestRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UCSBHelpRequestRepository extends CrudRepository<UCSBHelpRequest, Long> {
-  Iterable<UCSBHelpRequest> findAllByRequesterEmail(String requesterEmail);
+
 }
