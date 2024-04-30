a1f0272103fec9ec277116d362da19debf40ce18
Aneesh Agarwal <911aneesh@gmail.com>
2024-04-29 19:34:39 -0700

    slight change

--- src/main/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @Tag(name = "UCSBOrganization")
-@RequestMapping("/api/ucsborganizations")
+@RequestMapping("/api/ucsborganization")
 @RestController
 @Slf4j
 public class UCSBOrganizationController extends ApiController {
