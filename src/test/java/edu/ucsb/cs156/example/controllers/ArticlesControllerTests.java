62170f2b3bd3b63f763fa55b484231a07a57642a
Zhen Bi <zhenbi@csil.cs.ucsb.edu>
2024-05-01 00:56:59 -0700

    Controller post and get by June, passed test

--- src/test/java/edu/ucsb/cs156/example/controllers/ArticlesControllerTests.java
+++ src/test/java/edu/ucsb/cs156/example/controllers/ArticlesControllerTests.java
@@ -25,6 +25,8 @@ import java.time.LocalDateTime;
 
 import java.util.Optional;
 
+import java.net.URLDecoder;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
@@ -34,115 +36,115 @@ import static org.mockito.Mockito.when;
 
 @WebMvcTest(controllers = ArticlesController.class)
 @Import(TestConfig.class)
+public class ArticlesControllerTests extends ControllerTestCase {
 
-public class ArticlesControllerTests extends ControllerTestCase{
-    @MockBean
-    ArticlesRepository articlesRepository;
+        @MockBean
+        ArticlesRepository articlesRepository;
 
-    @MockBean
-    UserRepository userRepository;
+        @MockBean
+        UserRepository userRepository;
 
-    // Tests for GET /api/ucsbarticles/all
-    
-    @Test
-    public void logged_out_users_cannot_get_all() throws Exception {
-            mockMvc.perform(get("/api/ucsbarticles/all"))
-                            .andExpect(status().is(403)); // logged out users can't get all
-    }
+        // Tests for GET /api/ucsbdates/all
+        
+        @Test
+        public void logged_out_users_cannot_get_all() throws Exception {
+                mockMvc.perform(get("/api/articles/all"))
+                                .andExpect(status().is(403)); // logged out users can't get all
+        }
 
-    @WithMockUser(roles = { "USER" })
-    @Test
-    public void logged_in_users_can_get_all() throws Exception {
-            mockMvc.perform(get("/api/ucsbarticles/all"))
-                            .andExpect(status().is(200)); // logged
-    }
+        @WithMockUser(roles = { "USER" })
+        @Test
+        public void logged_in_users_can_get_all() throws Exception {
+                mockMvc.perform(get("/api/articles/all"))
+                                .andExpect(status().is(200)); // logged
+        }
 
-    @WithMockUser(roles = { "USER" })
-    @Test
-    public void logged_in_user_can_get_all_ucsbarticles() throws Exception {
+        @WithMockUser(roles = { "USER" })
+        @Test
+        public void logged_in_user_can_get_all_articles() throws Exception {
 
-            // arrange
-            LocalDateTime ldt1 = LocalDateTime.parse("2022-04-20T00:00:00");
+                // arrange
+                LocalDateTime ldt1 = LocalDateTime.parse("2022-04-20T00:00:00");
 
-            Articles ucsbArticle1 = Articles.builder()
-                            .title("Using testing-playground with React Testing Library")
-                            .url("https://dev.to/katieraby/using-testing-playground-with-react-testing-library-26j7")
-                            .explanation("Helpful when we get to front end development")
-                            .email("phtcon@ucsb.edu")
-                            .dateAdded(ldt1)
-                            .build();
+                Articles article1 = Articles.builder()
+                                .title("Using testing-playground with React Testing Library")
+                                .url("https://dev.to/katieraby/using-testing-playground-with-react-testing-library-26j7")
+                                .explanation("Helpful when we get to front end development")
+                                .email("phtcon@ucsb.edu")
+                                .dateAdded(ldt1)
+                                .build();
 
-            LocalDateTime ldt2 = LocalDateTime.parse("2022-04-19T00:00:00");
+                LocalDateTime ldt2 = LocalDateTime.parse("2022-04-19T00:00:00");
 
-            Articles ucsbArticle2 = Articles.builder()
-                            .title("Handy Spring Utility Classes")
-                            .url("https://twitter.com/maciejwalkowiak/status/1511736828369719300?t=gGXpmBH4y4eY9OBSUInZEg&s=09")
-                            .explanation("A lot of really useful classes are built into Spring")
-                            .email("phtcon@ucsb.edu")
-                            .dateAdded(ldt2)
-                            .build();
+                Articles article2 = Articles.builder()
+                                .title("Handy Spring Utility Classes")
+                                .url("https://twitter.com/maciejwalkowiak/status/1511736828369719300?t=gGXpmBH4y4eY9OBSUInZEg&s=09")
+                                .explanation("A lot of really useful classes are built into Spring")
+                                .email("phtcon@ucsb.edu")
+                                .dateAdded(ldt2)
+                                .build();
 
-            ArrayList<Articles> expectedArticles = new ArrayList<>();
-            expectedArticles.addAll(Arrays.asList(ucsbArticle1, ucsbArticle2));
+                ArrayList<Articles> expectedArticles = new ArrayList<>();
+                expectedArticles.addAll(Arrays.asList(article1, article2));
 
-            when(articlesRepository.findAll()).thenReturn(expectedArticles);
+                when(articlesRepository.findAll()).thenReturn(expectedArticles);
 
-            // act
-            MvcResult response = mockMvc.perform(get("/api/ucsbarticles/all"))
-                            .andExpect(status().isOk()).andReturn();
+                // act
+                MvcResult response = mockMvc.perform(get("/api/articles/all"))
+                                .andExpect(status().isOk()).andReturn();
 
-            // assert
+                // assert
 
-            verify(articlesRepository, times(1)).findAll();
-            String expectedJson = mapper.writeValueAsString(expectedArticles);
-            String responseString = response.getResponse().getContentAsString();
-            assertEquals(expectedJson, responseString);
-    }
+                verify(articlesRepository, times(1)).findAll();
+                String expectedJson = mapper.writeValueAsString(expectedArticles);
+                String responseString = response.getResponse().getContentAsString();
+                assertEquals(expectedJson, responseString);
+        }
 
-    // Tests for POST /api/ucsbdates/post...
+        // Tests for POST /api/articles/post...
 
-    @Test
-    public void logged_out_users_cannot_post() throws Exception {
-            mockMvc.perform(post("/api/ucsbarticles/post"))
-                            .andExpect(status().is(403));
-    }
+        @Test
+        public void logged_out_users_cannot_post() throws Exception {
+                mockMvc.perform(post("/api/articles/post"))
+                                .andExpect(status().is(403));
+        }
 
-    @WithMockUser(roles = { "USER" })
-    @Test
-    public void logged_in_regular_users_cannot_post() throws Exception {
-            mockMvc.perform(post("/api/ucsbarticles/post"))
-                            .andExpect(status().is(403)); // only admins can post
-    }
+        @WithMockUser(roles = { "USER" })
+        @Test
+        public void logged_in_regular_users_cannot_post() throws Exception {
+                mockMvc.perform(post("/api/articles/post"))
+                                .andExpect(status().is(403)); // only admins can post
+        }
 
-    @WithMockUser(roles = { "ADMIN", "USER" })
-    @Test
-    public void an_admin_user_can_post_a_new_ucsbarticles() throws Exception {
-            // arrange
-            // arrange
-            LocalDateTime ldt1 = LocalDateTime.parse("2022-04-20T00:00:00");
+        @WithMockUser(roles = { "ADMIN", "USER" })
+        @Test
+        public void an_admin_user_can_post_a_new_article() throws Exception {
+                // arrange
 
-            Articles ucsbArticle1 = Articles.builder()
-                            .title("Using testing-playground with React Testing Library")
-                            .url("https://dev.to/katieraby/using-testing-playground-with-react-testing-library-26j7")
-                            .explanation("Helpful when we get to front end development")
-                            .email("phtcon@ucsb.edu")
-                            .dateAdded(ldt1)
-                            .build();
+                LocalDateTime ldt1 = LocalDateTime.parse("2022-04-20T00:00:00");
 
-            when(articlesRepository.save(eq(ucsbArticle1))).thenReturn(ucsbArticle1);
+                Articles article1 = Articles.builder()
+                        .title("Article1")
+                        .url("www.fakewebsite/a1.com")
+                        .explanation("This is article 1")
+                        .email("zhenbi@ucsb.edu")
+                        .dateAdded(ldt1)
+                        .build();
 
-            // act
-            MvcResult response = mockMvc.perform(
-                            post("/api/ucsbarticles/post?title=Using testing-playground with React Testing Library&ucrl=https://dev.to/katieraby/using-testing-playground-with-react-testing-library-26j7&explanation=Helpful when we get to front end development&email=phtcon@ucsb.edudateAdded=2022-04-20T00:00:00")
-                                            .with(csrf()))
-                            .andExpect(status().isOk()).andReturn();
+                when(articlesRepository.save(eq(article1))).thenReturn(article1);
 
-            // assert
-            verify(articlesRepository, times(1)).save(ucsbArticle1);
-            String expectedJson = mapper.writeValueAsString(ucsbArticle1);
-            String responseString = response.getResponse().getContentAsString();
-            assertEquals(expectedJson, responseString);
-    }
+                // act
+                MvcResult response = mockMvc.perform(
+                                post("/api/articles/post?title=Article1&url=www.fakewebsite/a1.com&explanation=This is article 1&email=zhenbi@ucsb.edu&dateAdded=2022-04-20T00:00:00")
+                                .with(csrf()))
+                                .andExpect(status().isOk()).andReturn();
 
+                // assert
+                verify(articlesRepository, times(1)).save(article1);
+                String expectedJson = mapper.writeValueAsString(article1);
+                String responseString = response.getResponse().getContentAsString();
+                responseString = URLDecoder.decode(responseString, "ASCII");
+                assertEquals(expectedJson, responseString);
+        }
 
 }
